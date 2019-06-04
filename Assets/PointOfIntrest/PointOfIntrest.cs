7624025682b9503ad1030b862df834c8ebf2d342
dudemandando <38444209+dudemandando@users.noreply.github.com>
2019-06-04 14:56:42 -0700

    IconsWhite, Started POIManager & POI prefabs

---
+++ Assets/PointOfIntrest/PointOfIntrest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointOfIntrest : MonoBehaviour
+{
+
+    public Type pointType;
+    public GameObject terrainRefPoint; //Ghost reference point to be attached to the Terrain
+    private Image iconImage;
+    private Image bgImage;
+    public Color normalColor;
+    public Color highlightColor;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        iconImage = transform.Find("Canvas").Find("IconImage").GetComponent<Image>();
+        bgImage = transform.Find("Canvas").Find("Bg").GetComponent<Image>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void initPoint(Type initType)
+    {
+
+    }
+}

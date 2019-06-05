26f07532d3ffea81eabe217d563cb1e7021d874e
bChancellor <b.k.chancellor@gmail.com>
2019-06-05 15:19:41 -0700

    eye tracking added to prefab
    eye tracking added

---
+++ Assets/Scripts/ToggleIntrestPointCanvas.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleIntrestPointCanvas : MonoBehaviour
+{
+    public GameObject canvasToToggle;
+    public Color defaultImageBackgroundColor;
+    public Color highlightImageBackgroundColor;
+    public Image backgroundImage;
+
+    public void ToggleCanvas()
+    {
+        if (canvasToToggle.activeInHierarchy)
+            canvasToToggle.SetActive(false);
+        else
+            canvasToToggle.SetActive(true);
+    }
+
+    public void HighlightImageBackground()
+    {
+        backgroundImage.color = highlightImageBackgroundColor;
+    }
+
+    public void defaultImageBackground()
+    {
+        backgroundImage.color = defaultImageBackgroundColor;
+    }
+}

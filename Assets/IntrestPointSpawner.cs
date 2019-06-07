7aea5b96fd20baa10c77d2d2e260f149d0bba14f
bChancellor <b.k.chancellor@gmail.com>
2019-06-07 13:23:31 -0700

    fixed eye track errors

--- Assets/IntrestPointSpawner.cs
+++ Assets/IntrestPointSpawner.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class IntrestPointSpawner : MonoBehaviour
 {
     public Type disasterType;
+    public GameObject pointOfIntrestPrefab;
 
     void Start()
     {
-        GameObject tempPrefab = Instantiate(this.gameObject, this.transform.position, Quaternion.identity);
+        GameObject tempPrefab = Instantiate(pointOfIntrestPrefab, this.transform.position, Quaternion.identity);
         tempPrefab.transform.parent = this.transform;
         tempPrefab.GetComponent<PointOfIntrest>().pointType = disasterType;
         tempPrefab.GetComponent<PointOfIntrest>().initPoint(disasterType);
